717d8bb16594c6e24796579933044f442cf4bf68
Jacob <jacobpayne007@gmail.com>
2024-03-17 21:28:44 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+//step1
+const element = document.getElementById("main")
+element.remove();
+//step2
+const newHeader = document.createElement("h1");
+
+//step3
+newHeader.id = "victory"
+//step4
+newHeader.innerHTML = "Jacob is the champion"
+ewHeader.className = "victory";
